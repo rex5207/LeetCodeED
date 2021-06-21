472b925d6c760794cf924e61c920e0e35cbf9801
Rex Chen <abab5207@gmail.com>
2021-06-22 00:43:45 +0800

    002_AddTwoNumbers_LinkedList

---
+++ 002_AddTwoNumbers.cpp
@@ -0,0 +1,50 @@
+/***********************************************************************
+ *  https://leetcode.com/problems/add-two-numbers/
+ * 
+ *  Difficulty: Medium
+ * 
+ *  Related Topics: Linked List, Recursion
+ * 
+ *  Time complexity :  O(max(m,n)). Assume that mm and nn represents the length of l1 and l2 respectively
+ *
+ *  Runtime: 28 ms, faster than 70.53%
+ * 
+ *  Memory Usage: 71.4 MB, less than 43.09%
+ * 
+ *  Note: None
+ ***********************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    /* My solution */
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *ans = new ListNode(0);
+        ListNode *head = ans;
+        while(l1 || l2) {
+            if (l1) {
+                ans->val += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                ans->val += l2->val;
+                l2 = l2->next;
+            }
+            if (ans->val >= 10 || l1 || l2) {
+                ans->next = new ListNode(ans->val / 10);
+                ans->val %= 10;
+                ans = ans->next;
+            }
+        }
+        return head;
+    }
+};
